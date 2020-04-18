17f61cee8e9eb05c5b8ea52bec634430f92ff9c6
Michael Spikes <michael.spikes85@gmail.com>
2020-04-17 21:06:58 -0400

    finished the generatre password conditional. fully functional now. need check for characters from every seleceted chacter sets.

--- Develop/script.js
+++ Develop/script.js
@@ -1,44 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-if (generateBtn) {
-  var charSet = prompt(
-    "How many character do you want? Select between 8 and 128."
-  );
-  var upperAnswer = confirm("Do you want uppercase characters?");
-  var lowerAnswer = confirm("Do you want lowercase characters?");
-  var numericAnswer = confirm("Do you want numeric characters?");
-  var specialAnswer = confirm("Do you want special characters?");
-}
-
-// Write password to the #password input
-
 var upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
 var lowerCase = "qwertyuiopasdfghjklzxcvbnm";
 var numeric = "1234567890";
 var specialChar = "!@#$%^&*()";
-
 var allowed = "";
 
-if (upperAnswer) {
-  allowed += upperCase;
-  console.log(allowed);
-}
-
-if (lowerAnswer) {
-  allowed += lowerCase;
-  console.log(allowed);
-}
-
-if (numericAnswer) {
-  allowed += numeric;
-  console.log(allowed);
-}
-
-if (specialAnswer) {
-  allowed += specialChar;
-  console.log(allowed);
-}
+// Write password to the #password input
 
 function writePassword() {
   var password = generatePassword();
@@ -48,9 +16,54 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var userInput = prompt("Enter your password.");
+  var pass = "";
+  var charSet = 0;
+  while (charSet < 8 || charSet > 128) {
+    charSet = prompt(
+      "How many character do you want? Select between 8 and 128."
+    );
+    if (+charSet < 8 || +charSet > 128) {
+      alert("You must select a number between 8 and 128!");
+    }
+  }
 
-  return userInput;
+  var upperAnswer = confirm("Do you want uppercase characters?");
+  var lowerAnswer = confirm("Do you want lowercase characters?");
+  var numericAnswer = confirm("Do you want numeric characters?");
+  var specialAnswer = confirm("Do you want special characters?");
+
+  if (upperAnswer) {
+    allowed += upperCase;
+    console.log(allowed);
+  }
+
+  if (lowerAnswer) {
+    allowed += lowerCase;
+    console.log(allowed);
+  }
+
+  if (numericAnswer) {
+    allowed += numeric;
+    console.log(allowed);
+  }
+
+  if (specialAnswer) {
+    allowed += specialChar;
+    console.log(allowed);
+  }
+
+  if (!allowed.length) {
+    alert("You must select at least 1 character set!");
+    generatePassword();
+  }
+
+  for (let i = 0; i < allowed.length; i++) {
+    pass += allowed.charAt(Math.floor(Math.random() * allowed.length));
+  }
+
+  pass = pass.slice(0, +charSet);
+
+  return pass;
 }
 
 // Add event listener to generate button
